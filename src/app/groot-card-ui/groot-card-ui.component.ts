68362301c661248af57db6723f36ebc8ce1fe243
Akhlaque <akhlaquea01@gmail.com>
2025-07-28 00:04:55 +0530

    refactor: update GrootCardUi component to use dynamic inputs for image, title, and description

--- src/app/groot-card-ui/groot-card-ui.component.ts
+++ src/app/groot-card-ui/groot-card-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-groot-card-ui',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groot-card-ui.component.css']
 })
 export class GrootCardUiComponent implements OnInit {
+  @Input() imageUrl: string = 'assets/image.png';
+  @Input() title: string = 'GROOT';
+  @Input() description: string = 'This tree-like being branched out of his comfort zone to help the Guardians of the Galaxy keep the people of the universe safe.';
 
   constructor() { }
 
